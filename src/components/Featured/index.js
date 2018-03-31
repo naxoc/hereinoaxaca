5a2304e92f4d1a1763fd8b6cf883cb78f6dfce52
Camilla Krag Jensen <ckj@information.dk>
2018-03-31 10:44:06 -0600

    WIP

---
+++ src/components/Featured/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Featured = () => (
+  <div>
+    Featured
+  </div>
+);
+
+export default Featured
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,63 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import PropTypes from "prop-types";
+import get from "lodash/get";
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+
+  static propTypes = {
+    data: PropTypes.shape({
+      allMarkdownRemark: PropTypes.shape({
+        edges: PropTypes.arrayOf(
+          PropTypes.shape({
+            node: PropTypes.shape({
+              frontmatter: PropTypes.shape({
+                title: PropTypes.string.isRequired,
+              }),
+              fields: PropTypes.shape({
+                slug: PropTypes.string.isRequired,
+              }),
+            }),
+          }).isRequired
+        ),
+      }),
+    }),
+  };
+
+  render() {
+    const markdown = get(this, 'props.data.allMarkdownRemark');
+    return (
+      <div>
+        <h1>Here in Oaxaca</h1>
+        {markdown.edges.map(({ node }, idx) => (
+          <div key={idx}>
+            <p><a href={node.fields.slug}>{node.frontmatter.title}</a></p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default IndexPage
+
+export const allQuery = graphql`
+query allQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`;
